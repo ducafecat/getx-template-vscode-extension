c5fd7589f27ac045532fc639b1ab0daf3497cee9
ducafecat <ducafecat@gmail.com>
2021-06-04 16:03:53 +0800

    add -> 生成代码

---
+++ src/extension.ts
@@ -0,0 +1,35 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { newGetxPage } from './commands/new-getx-page.command';
+// import { commands, ExtensionContext, languages, workspace } from "vscode";
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	// console.log('Congratulations, your extension "getx-template" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	// let disposable = vscode.commands.registerCommand('getx-template.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
+
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from getx template!');
+	// });
+	// context.subscriptions.push(disposable);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand("extension.new-getx-page", newGetxPage),
+	);
+
+
+
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
---
+++ src/templates/getx-page.template.ts
@@ -0,0 +1,279 @@
+import * as changeCase from "change-case";
+import { existsSync, lstatSync, writeFile } from "fs";
+
+export function indexTemplate(pageName: string, targetDirectory: string) {
+  const pascalCaseName = changeCase.pascalCase(pageName.toLowerCase());
+  const snakeCaseName = changeCase.snakeCase(pageName.toLowerCase());
+  const targetPath = `${targetDirectory}/${pageName}/index.dart`;
+  const template = `library ${snakeCaseName};
+
+export './state.dart';
+export './controller.dart';
+export './bindings.dart';
+export './view.dart';
+`;
+
+  return new Promise(async (resolve, reject) => {
+    writeFile(
+      targetPath,
+      template,
+      "utf8",
+      (error) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve;
+      }
+    );
+  });
+}
+
+export function stateTemplate(pageName: string, targetDirectory: string) {
+  const pascalCaseName = changeCase.pascalCase(pageName.toLowerCase());
+  const snakeCaseName = changeCase.snakeCase(pageName.toLowerCase());
+  const targetPath = `${targetDirectory}/${pageName}/state.dart`;
+  const template = `import 'package:get/get.dart';
+
+class ${pascalCaseName}State {
+  // title
+  final _title = "".obs;
+  set title(value) => this._title.value = value;
+  get title => this._title.value;
+}
+`;
+
+  return new Promise(async (resolve, reject) => {
+    writeFile(
+      targetPath,
+      template,
+      "utf8",
+      (error) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve;
+      }
+    );
+  });
+}
+
+export function controllerTemplate(pageName: string, targetDirectory: string) {
+  const pascalCaseName = changeCase.pascalCase(pageName.toLowerCase());
+  const snakeCaseName = changeCase.snakeCase(pageName.toLowerCase());
+  const targetPath = `${targetDirectory}/${pageName}/controller.dart`;
+  const template = `import 'package:get/get.dart';
+
+import 'index.dart';
+
+class ${pascalCaseName}Controller extends GetxController {
+  ${pascalCaseName}Controller();
+
+  /// 响应式成员变量
+
+  final state = ${pascalCaseName}State();
+
+  /// 成员变量
+
+  /// 事件
+
+  // tap
+  void handleTap(int index) {
+    Get.snackbar(
+      "标题",
+      "消息",
+    );
+  }
+
+  /// 生命周期
+
+  ///在 widget 内存中分配后立即调用。
+  ///你可以用它来为控制器初始化 initialize 一些东西。
+  @override
+  void onInit() {
+    super.onInit();
+    // new 对象
+    // 初始静态数据
+  }
+
+  ///在 onInit() 之后调用 1 帧。这是进入的理想场所
+  ///导航事件，例如 snackbar、对话框或新route，或
+  ///async 异步请求。
+  @override
+  void onReady() {
+    super.onReady();
+    // async 拉取数据
+  }
+
+  ///在 [onDelete] 方法之前调用。 [onClose] 可能用于
+  ///处理控制器使用的资源。就像 closing events 一样，
+  ///或在控制器销毁之前的流。
+  ///或者处置可能造成一些内存泄漏的对象，
+  ///像 TextEditingControllers、AnimationControllers。
+  ///将一些数据保存在磁盘上也可能很有用。
+  @override
+  void onClose() {
+    super.onClose();
+    // 1 stop & close 关闭对象
+    // 2 save 持久化数据
+  }
+
+  ///dispose 释放内存
+  @override
+  void dispose() {
+    super.dispose();
+    // dispose 释放对象
+  }
+}
+`;
+
+  return new Promise(async (resolve, reject) => {
+    writeFile(
+      targetPath,
+      template,
+      "utf8",
+      (error) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve;
+      }
+    );
+  });
+}
+
+export function bindingsTemplate(pageName: string, targetDirectory: string) {
+  const pascalCaseName = changeCase.pascalCase(pageName.toLowerCase());
+  const snakeCaseName = changeCase.snakeCase(pageName.toLowerCase());
+  const targetPath = `${targetDirectory}/${pageName}/bindings.dart`;
+  const template = `import 'package:get/get.dart';
+
+import 'controller.dart';
+
+class ${pascalCaseName}Binding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<${pascalCaseName}Controller>(() => ${pascalCaseName}Controller());
+  }
+}
+`;
+
+  return new Promise(async (resolve, reject) => {
+    writeFile(
+      targetPath,
+      template,
+      "utf8",
+      (error) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve;
+      }
+    );
+  });
+}
+
+export function viewTemplate(pageName: string, targetDirectory: string) {
+  const pascalCaseName = changeCase.pascalCase(pageName.toLowerCase());
+  const snakeCaseName = changeCase.snakeCase(pageName.toLowerCase());
+  const targetPath = `${targetDirectory}/${pageName}/view.dart`;
+  const template = `import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'index.dart';
+import 'widgets/widgets.dart';
+
+class ${pascalCaseName}Page extends GetView<${pascalCaseName}Controller> {
+  // 内容页
+  Widget _buildView() {
+    return HellowordWidget();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildView(),
+    );
+  }
+}
+`;
+
+  return new Promise(async (resolve, reject) => {
+    writeFile(
+      targetPath,
+      template,
+      "utf8",
+      (error) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve;
+      }
+    );
+  });
+}
+
+export function widgetsTemplate(pageName: string, targetDirectory: string) {
+  const pascalCaseName = changeCase.pascalCase(pageName.toLowerCase());
+  const snakeCaseName = changeCase.snakeCase(pageName.toLowerCase());
+  const targetPath = `${targetDirectory}/${pageName}/widgets/widgets.dart`;
+  const template = `library widgets;
+
+export './helloword.dart';
+`;
+
+  return new Promise(async (resolve, reject) => {
+    writeFile(
+      targetPath,
+      template,
+      "utf8",
+      (error) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve;
+      }
+    );
+  });
+}
+
+export function widgetsHelloWordTemplate(pageName: string, targetDirectory: string) {
+  const pascalCaseName = changeCase.pascalCase(pageName.toLowerCase());
+  const snakeCaseName = changeCase.snakeCase(pageName.toLowerCase());
+  const targetPath = `${targetDirectory}/${pageName}/widgets/helloword.dart`;
+  const template = `import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../index.dart';
+
+/// hellowrod
+class HellowordWidget extends GetView<${pascalCaseName}Controller> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Obx(() => Text(controller.state.title)),
+    );
+  }
+}
+`;
+
+  return new Promise(async (resolve, reject) => {
+    writeFile(
+      targetPath,
+      template,
+      "utf8",
+      (error) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve;
+      }
+    );
+  });
+}
